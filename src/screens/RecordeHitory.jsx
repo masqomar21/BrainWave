3b605086768fabfe0bdd0b452ab8ae53bb47deec
masqomar21 <mqomarudin0480@gmail.com>
2024-02-29 14:34:31 +0700

    (fix) - eslint fix

---
+++ src/screens/RecordeHitory.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function RecordeHitory() {
+  return (
+      <View>
+          <Text>RecordeHitory</Text>
+      </View>
+  )
+}
